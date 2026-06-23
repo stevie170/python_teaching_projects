a689b10931646c07f0494bb9cece3e80c96296d3
Stevie <65572293+stevie170@users.noreply.github.com>
2026-06-23 09:28:58 -0700

    Add package example with wait function and prints

---
+++ packages_example.py
@@ -0,0 +1,16 @@
+import time
+
+def wait():
+  time.sleep(1)
+
+
+print('let\'s print something')
+wait()
+num = 5
+print(num)
+wait()
+print('this is the last line')
+wait()
+print('jk one more')
+
+
