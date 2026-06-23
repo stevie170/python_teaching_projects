09d1cc951cad22807f20d653233968f6b2e7fb55
Stevie <65572293+stevie170@users.noreply.github.com>
2026-06-23 09:28:04 -0700

    Add initial Alien Translator script
    Initial commit of the Alien Translator script with prompts for user input and suggestions for enhancements.

---
+++ alien_translator_challenge.py
@@ -0,0 +1,15 @@
+# Alien Translator
+"""
+Let's introduce an alien from another planet.
+"""
+
+# Use input() to ask for the Alien's name and planet and assign them to variables
+
+
+# Use print and f-strings to introduce the alien using the name and planet that they entered
+
+
+# Now upgrade your alien script on your own to make it more fun, expressive, or personalized!
+# Ideas:
+# What might your alien say instead of hello?
+# What kind of personality does your alien have? Loud? Shy? Mysterious?
